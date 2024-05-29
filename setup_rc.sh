9fa99c1f1e726382531b3ff0ebb892319a9bdf74
slorello89 <steve.lorello@redis.com>
2024-05-29 13:14:17 -0400

    modifying redis connect script

--- setup_rc.sh
+++ setup_rc.sh
@@ -5,8 +5,18 @@ chmod +w ./jars/Trades2Geode-1.0.jar
 
 docker run \
 -it --rm --privileged=true \
---name redis-connect-$(hostname) \
+--name redis-connect-$(hostname)-1 \
 -v $(pwd)/config:/opt/redislabs/redis-connect/config \
 -v $(pwd)/jars:/opt/redislabs/redis-connect/extlib \
 --net host \
+-p 8241:8242 \
+redislabs/redis-connect start
+
+docker run \
+-it --rm --privileged=true \
+--name redis-connect-$(hostname)-2 \
+-v $(pwd)/config:/opt/redislabs/redis-connect/config \
+-v $(pwd)/jars:/opt/redislabs/redis-connect/extlib \
+--net host \
+-p 8242:8242 \
 redislabs/redis-connect start
